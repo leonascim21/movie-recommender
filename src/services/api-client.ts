4b03f14e159ed03c11bbd886cc36321d3089e9b8
Leo <leonascim21@gmail.com>
2024-06-04 19:20:43 -0400

    removed console.log statement

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
-const apiKey = import.meta.env.VITE_API_KEY;
-console.log(apiKey)
-
 export default axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params:{
-        api_key: apiKey,
+        api_key: import.meta.env.VITE_API_KEY,
     }
 });
