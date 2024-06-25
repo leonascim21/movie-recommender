b78cbf39f3a2cfaa71e3a0a47851ebefcc248f5d
Leo <leonascim21@gmail.com>
2024-06-25 14:45:20 -0400

    new small NavBar component for mobile screens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,75 +1,11 @@
-import {
-  Box,
-  HStack,
-  Image,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  Avatar,
-} from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
-import ColorModeSwitch from "./ColorModeSwitch";
-import { useNavigate } from "react-router-dom";
-import SearchBar from "./SearchBar";
-import { useAuth } from "../hooks/useAuth";
+import { useMediaQuery } from "@chakra-ui/react";
+import NavBarBig from "./NavBarBig";
+import NavBarSmall from "./NavBarSmall";
 
 const NavBar = () => {
-  const { currentUser, logout } = useAuth();
-  const navigate = useNavigate();
+  const [isLargeScreen] = useMediaQuery("(min-width: 700px)");
 
-  const handleClickHome = () => {
-    navigate(`/`);
-  };
-
-  const handleLogin = () => {
-    navigate("/login");
-  };
-
-  const handleLogout = async () => {
-    await logout();
-    navigate(`/`);
-  };
-
-  const handleLikedMovies = () => {
-    navigate(`/liked-movies`);
-  };
-
-  return (
-    <Box width="90%" padding="10px">
-      <HStack padding="10px">
-        <Image
-          cursor="pointer"
-          onClick={handleClickHome}
-          src={logo}
-          boxSize="90px"
-        />
-        <SearchBar />
-        {currentUser ? (
-          <Menu>
-            <MenuButton
-              as={Button}
-              background="transparent"
-              _hover={{ background: "transparent" }}
-              _active={{ background: "transparent" }}
-            >
-              <Avatar size="sm" name={currentUser.email || "User"} />
-            </MenuButton>
-            <MenuList>
-              <MenuItem onClick={handleLikedMovies}>Liked Movies</MenuItem>
-              <MenuItem onClick={handleLogout}>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        ) : (
-          <Button onClick={handleLogin} colorScheme="green" m={7}>
-            Login
-          </Button>
-        )}
-        <ColorModeSwitch />
-      </HStack>
-    </Box>
-  );
+  return isLargeScreen ? <NavBarBig /> : <NavBarSmall />;
 };
 
 export default NavBar;
---
+++ src/components/NavBarSmall.tsx
@@ -0,0 +1,106 @@
+import {
+  Box,
+  HStack,
+  Image,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerCloseButton,
+  IconButton,
+  Avatar,
+  useDisclosure,
+  MenuButton,
+  Menu,
+  MenuItem,
+  MenuList,
+  DrawerOverlay,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import SearchBar from "./SearchBar";
+
+const NavBarSmall = () => {
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleClickHome = () => {
+    navigate(`/`);
+    onClose();
+  };
+
+  const handleLogin = () => {
+    navigate("/login");
+    onClose();
+  };
+
+  const handleLogout = async () => {
+    await logout();
+    navigate(`/`);
+    onClose();
+  };
+
+  const handleLikedMovies = () => {
+    navigate(`/liked-movies`);
+    onClose();
+  };
+
+  return (
+    <Box width="100%" padding="10px">
+      <HStack justifyContent="space-between" padding="10px">
+        <IconButton
+          icon={<HamburgerIcon />}
+          variant="outline"
+          onClick={onOpen}
+          aria-label={""}
+        />
+        <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody>
+              <Box pt={10}>
+                <SearchBar />
+              </Box>
+              <Box pt={7} pl={2}>
+                <ColorModeSwitch />
+              </Box>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+        <Image
+          cursor="pointer"
+          onClick={handleClickHome}
+          src={logo}
+          boxSize="90px"
+        />
+        {currentUser ? (
+          <Menu>
+            <MenuButton
+              as={Button}
+              background="transparent"
+              _hover={{ background: "transparent" }}
+              _active={{ background: "transparent" }}
+            >
+              <Avatar size="sm" name={currentUser.email || "User"} />
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={handleLikedMovies}>Liked Movies</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        ) : (
+          <Button onClick={handleLogin} colorScheme="green" m={7}>
+            Login
+          </Button>
+        )}
+      </HStack>
+    </Box>
+  );
+};
+
+export default NavBarSmall;
