d27f25a838c564393aac3b7065017db301263460
Leo <leonascim21@gmail.com>
2024-06-25 11:46:53 -0400

    Add genre name to genre page

--- src/pages/GenrePage.tsx
+++ src/pages/GenrePage.tsx
@@ -4,11 +4,34 @@ import NavBar from "../components/NavBar";
 import GenreList from "../components/GenreList";
 import MovieGrid from "../components/MovieGrid";
 import GenreListModal from "../components/GenreListModal";
-import { Button, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import { Button, Flex, Grid, GridItem, Heading, Show } from "@chakra-ui/react";
+
+const genres = {
+  "28": "Action",
+  "12": "Adventure",
+  "16": "Animation",
+  "35": "Comedy",
+  "80": "Crime",
+  "99": "Documentary",
+  "18": "Drama",
+  "10751": "Family",
+  "14": "Fantasy",
+  "36": "History",
+  "27": "Horror",
+  "10402": "Music",
+  "9648": "Mystery",
+  "10749": "Romance",
+  "878": "Science Fiction",
+  "10770": "TV Movie",
+  "53": "Thriller",
+  "10752": "War",
+  "37": "Western",
+};
 
 const GenrePage = () => {
   const { genreId } = useParams();
   const [page, setPage] = useState(1);
+  const genreName = genres[genreId as keyof typeof genres] || "Genre Not Found";
 
   const loadMoreMovies = () => {
     setPage((prevPage) => prevPage + 1);
@@ -37,6 +60,7 @@ const GenrePage = () => {
           </GridItem>
         </Show>
         <GridItem area="main">
+          <Heading pl={10}>{genreName}</Heading>
           <MovieGrid endpoint="/discover/movie" genre={genreId} page={page} />
         </GridItem>
       </Grid>
